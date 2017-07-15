ff50d514a0237f2721e7bdf26d4387192569d3e3
Nicole Sattler <nicole.sattler@live.com>
2017-07-15 20:15:45 +0000

    Added posts view

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,26 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Look at this!", author: "Bob"},
+        {title: "My new kitty", author: "Amanda"},
+        {title: "What's 2 + 2?", author: "Billy"}
+        ];
+        
+    res.render("posts", {posts: posts});    
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
