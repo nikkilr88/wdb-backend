861aee262feb6abe6b7975e67ca2171efcc7943a
Nicole Sattler <nicole.sattler@live.com>
2017-07-28 15:49:28 +0000

    Added in comments

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,30 +2,21 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
-    Campground = require("./models/campground");
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds");
     
 mongoose.connect("mongodb://localhost/yelp-camp", {useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// Campground.create({
-//     name: "Mountain Goat's Rest",
-//     image: "https://farm7.staticflickr.com/6186/6090714876_44d269ed7e.jpg",
-//     description: "I didn't see any goats!"
-// }, function(err, campground){
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log("Campground Added!");
-//         console.log(campground);
-//     }
-// });
+seedDB();
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+
+//Index
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, campgrounds){
         if(err){
@@ -37,13 +28,15 @@ app.get("/campgrounds", function(req, res){
     
 });
 
+//New
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
+//Show
 app.get("/campgrounds/:id", function(req, res){
     //Find campground with provided ID
-    Campground.findById(req.params.id, function(err, campground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, campground){
         if(err){
             console.log(err);
         } else {
@@ -52,6 +45,7 @@ app.get("/campgrounds/:id", function(req, res){
     });
 });
 
+//Create
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
@@ -67,6 +61,7 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//Start server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started!");
 });
