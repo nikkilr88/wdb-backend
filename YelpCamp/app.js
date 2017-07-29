30692ba9ee7ca57941b22b9f67e84bdf6bb5106b
Nicole Sattler <nicole.sattler@live.com>
2017-07-29 17:58:15 +0000

    Served public directory

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -12,6 +12,7 @@ mongoose.connect("mongodb://localhost/yelp-camp", {useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
 seedDB();
 
 app.get("/", function(req, res){
