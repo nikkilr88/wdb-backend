d8d86091bcaaa9019078e0ff1a4060e1d80b463a
Nicole Sattler <nicole.sattler@live.com>
2017-08-08 20:49:21 +0000

    Added middleware to check if user is logged in

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -85,7 +85,7 @@ app.post("/campgrounds", function(req, res){
 // Comments Routes
 //===================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -96,7 +96,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -157,6 +157,14 @@ app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/campgrounds");
 });
+
+//Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 //Start server
 app.listen(process.env.PORT, process.env.IP, function(){
