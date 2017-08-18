8d30b0959fb9661e90800ff1370942ecef78cee5
Nicole Sattler <nicole.sattler@live.com>
2017-08-18 19:40:31 +0000

    Added route prefixes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -38,9 +38,9 @@ app.use(function(req, res, next){
     next();
 });
 
-app.use(authRoutes);
-app.use(campgroundRoutes);
-app.use(commentRoutes);
+app.use("/", authRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 //Start server
 app.listen(process.env.PORT, process.env.IP, function(){
